8f2bde17891517ab81748efa3e21deac28ab64bf
Harsh_Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-06-18 10:13:36 +0530

    Update frontend configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
